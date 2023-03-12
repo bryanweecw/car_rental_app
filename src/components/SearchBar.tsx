f261d310783335744d5fbd202a08345c5542cb86
Wee Chin Wei, Bryan <bryan.wee@u.yale-nus.edu.sg>
2023-03-12 23:25:06 +0800

    CSS changes

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,31 +1,31 @@
-import React from 'react';
+import React from "react";
 
 interface SearchBarProps {
-    setQuery: (value: string) => void; // specify the type of setQuery prop
-    query: string;
-  }
+  setQuery: (value: string) => void; // specify the type of setQuery prop
+  query: string;
+}
 
 const SearchBar = ({ setQuery, query }: SearchBarProps) => {
-    function handleOnSearch(event: React.ChangeEvent<HTMLInputElement>){
-        const { value } = event.currentTarget;
-        setQuery(value)
-    }
-   return (
-       <div className="p-8">
-           <div className="bg-white flex items-center rounded-full shadow-xl">
-               <input name="search"
-                   type="search"
-                   placeholder="Search for a car. Ex: Toyota Corolla"
-                   autoComplete="off"
-                   className="rounded-l-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none"
-                   id="search"
-                   value = { query }
-                   onChange = { handleOnSearch } />
-               <div className="p-4">
-               </div>
-           </div>
-       </div>
-   );
-}
+  function handleOnSearch(event: React.ChangeEvent<HTMLInputElement>) {
+    const { value } = event.currentTarget;
+    setQuery(value);
+  }
+  return (
+    <div className="p-8">
+      <div className="flex items-center bg-white">
+        <input
+          name="search"
+          type="search"
+          placeholder="Search for a car. Ex: Toyota Corolla"
+          autoComplete="off"
+          className="w-full rounded-full py-4 px-6 leading-tight text-gray-700 shadow-xl focus:outline-none"
+          id="search"
+          value={query}
+          onChange={handleOnSearch}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default SearchBar;
