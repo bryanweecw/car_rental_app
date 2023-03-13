1d3373dc7a069ba683bbf4e9fa743b5e0f35b4e7
ryanlsh <ryan@u.yale-nus.edu.sg>
2023-03-13 13:57:15 +0800

    added escape to ' characters in contactpage

--- src/components/contactpage.tsx
+++ src/components/contactpage.tsx
@@ -40,7 +40,7 @@ export default function ContactPage() {
       <div className="mx-auto max-w-2xl text-center">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact Us</h2>
         <p className="mt-2 text-lg font-bold sm:text-3xl leading-8 text-gray-600">
-          We'd Love to Hear From You
+          We&apos;d Love to Hear From You
         </p>
       </div>
       <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
@@ -182,7 +182,7 @@ export default function ContactPage() {
             type="submit"
             className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Let's talk
+            Let&apos;s talk
           </button>
         </div>
       </form>
