7b4ac93bfe6a1c694105fa55d116d7cc53108df3
Wee Chin Wei, Bryan <bryan.wee@u.yale-nus.edu.sg>
2023-03-20 15:49:24 +0800

    updatehireagreement mvp

--- src/components/StaffHireAgreementView.tsx
+++ src/components/StaffHireAgreementView.tsx
@@ -3,6 +3,14 @@ import { api } from "~/utils/api";
 
 interface SHAVProps {
   agreementData: AgreementInfo;
+  onAgreementChange: () => void;
+}
+
+interface SanitisedUpdateQueryType {
+  date_end: string;
+  date_start: string;
+  vehicle_registration_number: string;
+  hire_agreement_id: string;
 }
 
 interface CarType {
@@ -45,13 +53,27 @@ type AgreementInfo = {
   vehicle_registration_number: string | null;
 };
 
-export default function StaffHireAgreementView({ agreementData }: SHAVProps) {
+export default function StaffHireAgreementView({
+  agreementData,
+  onAgreementChange,
+}: SHAVProps) {
   const readOnly = true;
   const infoData = agreementData;
   const [formData, setFormData] = useState<AgreementInfo>(infoData);
   const [availableVehicles, setAvailableVehicles] = useState<Set<string>>();
   const [availableVehicleDetails, setAvailableVehicleDetails] =
     useState<AvailableVehicleDetailType>({});
+
+  const { mutate, isLoading } =
+    api.hireAgreementUpdateMutation.HireAgreementUpdate.useMutation({
+      onSuccess: (res) => {
+        onAgreementChange();
+        console.log(res);
+      },
+      onError: (err) => {
+        console.log(err);
+      },
+    });
 
   const infoDataOrFormDataStartDate = (
     infoData: AgreementInfo,
@@ -94,7 +116,8 @@ export default function StaffHireAgreementView({ agreementData }: SHAVProps) {
 
   useEffect(() => {
     if (postQuery.status == "success" && postQuery2.status == "success") {
-      const vehicleSet = new Set<string>(); //local set of unavailable vehicles
+      const vehicleSet = new Set<string>();
+      //local set of unavailable vehicles for the given date range to be populated later
       (
         postQuery.data as unknown as {
           vehicle: BriefCarType;
@@ -102,33 +125,52 @@ export default function StaffHireAgreementView({ agreementData }: SHAVProps) {
           date_start: string;
           date_end: string;
         }[]
-      ).map((vehicle) => {
-        if (vehicle.vehicle.outlet_id == outlet_id) {
-          vehicleSet.add(vehicle.vehicle.vehicle_registration_number);
-        }
-      });
-      console.log(vehicleSet);
+      ) // this is to convert the data to the correct type, can be refactored out
+        .map((vehicle) => {
+          if (vehicle.vehicle.outlet_id == outlet_id) {
+            vehicleSet.add(vehicle.vehicle.vehicle_registration_number);
+          } // this is to check if the vehicle is in the same outlet and add it to the set if it is
+        });
+
+      // create local set of available vehicles
       const localAvailableVehicleSet = new Set<string>();
+      // create local set of available vehicle details to be populated later
       const localAvailableVehicleDetails: AvailableVehicleDetailType = {};
+
+      // populate the local set of available vehicles and available vehicle details
       (postQuery2.data as CarType[]).map((vehicle) => {
         if (
           vehicle.outlet_id == outlet_id &&
           vehicle.vehicle_registration_number &&
           !vehicleSet.has(vehicle.vehicle_registration_number)
+          // this is to check if the vehicle is available and is in the same outlet
         ) {
           localAvailableVehicleSet.add(vehicle.vehicle_registration_number);
+          // this is to add the vehicle to the set of available vehicles
           localAvailableVehicleDetails[vehicle.vehicle_registration_number] = {
             vehicle_make: vehicle.vehicle_make,
             vehicle_model: vehicle.vehicle_model,
             vehicle_registration_number: vehicle.vehicle_registration_number,
             outlet_id: vehicle.outlet_id,
           };
+          // this is to add the vehicle details to the set of available vehicle details
         }
       });
+
+      // perform set states to update the available vehicles and available vehicle details
       setAvailableVehicles(localAvailableVehicleSet);
       setAvailableVehicleDetails(localAvailableVehicleDetails);
     }
-  }, [postQuery.data, postQuery2.data, formData.date_start, formData.date_end]);
+  }, [
+    postQuery.data,
+    postQuery2.data,
+    formData.date_start,
+    formData.date_end,
+    postQuery.status,
+    postQuery2.status,
+    outlet_id,
+  ]);
+
   if (postQuery.status == "success" && postQuery2.status == "success") {
     return (
       <div className="mx-auto border p-5">
@@ -231,12 +273,21 @@ export default function StaffHireAgreementView({ agreementData }: SHAVProps) {
                 </option>
               )}
               {availableVehicleDetails &&
-                Object.values(availableVehicleDetails).map((vehicle) => (
-                  <option key={vehicle.vehicle_registration_number}>
-                    {vehicle.vehicle_registration_number} (
-                    {vehicle.vehicle_make} {vehicle.vehicle_model})
-                  </option>
-                ))}
+                Object.values(availableVehicleDetails).map((vehicle) =>
+                  availableVehicles?.has(
+                    infoData.vehicle_registration_number as string
+                  ) &&
+                  vehicle.vehicle_registration_number ==
+                    infoData.vehicle_registration_number ? null : (
+                    <option
+                      key={vehicle.vehicle_registration_number}
+                      value={vehicle.vehicle_registration_number}
+                    >
+                      {vehicle.vehicle_registration_number} (
+                      {vehicle.vehicle_make} {vehicle.vehicle_model})
+                    </option>
+                  )
+                )}
             </select>
           </div>
         </div>
@@ -244,6 +295,35 @@ export default function StaffHireAgreementView({ agreementData }: SHAVProps) {
           type="button"
           onClick={(e) => {
             e.preventDefault();
+
+            //prepare the data to be sent to the backend
+
+            const queryInputData = {
+              date_end: formData.date_end,
+              date_start: formData.date_start,
+              vehicle_registration_number: formData.vehicle_registration_number,
+              hire_agreement_id: formData.hire_agreement_id.toString(),
+            };
+
+            // const {
+            //   client,
+            //   client_uid,
+            //   rental_cost,
+            //   staff,
+            //   staff_uid,
+            //   vehicle,
+            //   ...queryInputData
+            // } = formData;
+            console.log(queryInputData);
+
+            if (
+              queryInputData.date_end != null &&
+              queryInputData.date_start != null &&
+              queryInputData.vehicle_registration_number != null &&
+              queryInputData.hire_agreement_id != null
+            ) {
+              mutate(queryInputData as SanitisedUpdateQueryType);
+            }
           }}
           className="self-right my-10 w-full rounded-md bg-indigo-600 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
