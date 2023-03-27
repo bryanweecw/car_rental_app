15169d494b11ea13c7327becc6b454aa877abe40
ryanlsh <ryan@u.yale-nus.edu.sg>
2023-03-27 23:17:00 +0800

    now cursor changes to finger when hovering in staff dashboard

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -74,7 +74,7 @@ export default function TabBar() {
               className={classNames(
                 tab.value == activeTab
                   ? "text-gray-900"
-                  : "text-gray-500 hover:text-gray-700",
+                  : "text-gray-500 hover:cursor-pointer hover:text-gray-700",
                 tabIdx === 0 ? "rounded-l-lg" : "",
                 tabIdx === tabs.length - 1 ? "rounded-r-lg" : "",
                 "group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10"
